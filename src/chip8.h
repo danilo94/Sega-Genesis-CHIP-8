681bead48df8fa57269d4f68646ddb146bdecbfd
Danilo Almeida <danilooalmeida94@gmail.com>
2021-10-17 15:23:52 -0300

    Update chip8.h

--- src/chip8.h
+++ src/chip8.h
@@ -1,3 +1,16 @@
+	///////////////////////////////////////////////////////////////////////////////
+// Project description
+// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+// Name: myChip8
+//
+// Author: Laurence Muller
+// Contact: laurence.muller@gmail.com
+//
+// License: GNU General Public License (GPL) v2 
+// ( http://www.gnu.org/licenses/old-licenses/gpl-2.0.html )
+//
+// Copyright (C) 2011 Laurence Muller / www.multigesture.net
+///////////////////////////////////////////////////////////////////////////////
 #ifndef CHIP8_INCLUDE_H
 #define CHIP8_INCLUDE_H
 #include <genesis.h>
